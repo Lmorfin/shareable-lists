806dce840a44299b211dfa260a155e0853575137
Luis Morfin <luismorfin@Luiss-MacBook-Pro.local>
2023-09-07 21:55:43 -0700

    env

--- frontend/src/config/env.js
+++ frontend/src/config/env.js
@@ -1,13 +1,12 @@
 const getUrlEndpoint = () => {
-    let url = "";
-    if (env === "local") {
-      url = "http://localhost:4000";
-    } else {
-      url = "test.....com";
-    }
-    return url;
-  };
-  
-  export const env = "local";
-  export const url = getUrlEndpoint();
-  
+  let url = "";
+  if (env === "local") {
+    url = "http://localhost:4000";
+  } else {
+    url = "http://54.193.34.31:4000";
+  }
+  return url;
+};
+
+export const env = "production";
+export const url = getUrlEndpoint();
